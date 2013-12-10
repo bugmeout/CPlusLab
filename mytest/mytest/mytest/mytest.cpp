b1770daa2859de021deeb8273c9121b2d1677313
bugmeout <hicode@foxmail.com>
2013-12-10 20:06:17 +0800

    first commit

---
+++ mytest/mytest/mytest/mytest.cpp
@@ -0,0 +1,17 @@
+// mytest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a = 0;
+	int b = 2;
+	cout<<a+b<<endl;
+	getchar();
+	return 0;
+}
+
